57429b2aa20a4c6d9194d08f567e875d65fbe558
Jeff Kluge <jeffkl@microsoft.com>
2016-11-10 11:09:53 -0800

    Initial commit

---
+++ src/ConsoleApplication/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Build.Evaluation;
+using System;
+using System.IO;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.Error.WriteLine("Please specify the full path to an MSBuild project to load.");
+                return 1;
+            }
+
+            string projectPath = Path.GetFullPath(args[0]);
+
+            if (!File.Exists(projectPath))
+            {
+                Console.Error.WriteLine($"The project file '{projectPath}' does not exist or is inaccessible.");
+                return 2;
+            }
+
+            Project project = new Project(projectPath);
+
+            Console.WriteLine($"Successfully loaded project file '{projectPath}'.");
+
+            while (true)
+            {
+                Console.Write("Please entry a property to read, or press Enter to exit: ");
+
+                string propertyName = Console.ReadLine();
+
+                if (String.IsNullOrWhiteSpace(propertyName))
+                {
+                    break;
+                }
+
+                Console.WriteLine();
+
+                Console.WriteLine($"  {propertyName} = '{project.GetPropertyValue(propertyName)}'");
+
+                Console.WriteLine();
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ src/ConsoleApplication/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApplication")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0634d3a3-df3d-45d9-af62-218d9c3b0f69")]

80762ea76c72b3db10262d199fcf82180e6f2f23
Jeff Kluge <jeffkl@microsoft.com>
2016-12-15 08:09:52 -0800

    Cross-platform task library

---
+++ src/MyTask/MyTask.cs
@@ -0,0 +1,16 @@
+﻿using Task = Microsoft.Build.Utilities.Task;
+
+namespace MyTasks
+{
+    public class MyTask : Task
+    {
+        public string Param1 { get; set; }
+
+        public override bool Execute()
+        {
+            Log.LogMessage($"Hello from MyTask. Param1 = '{Param1}'");
+
+            return !Log.HasLoggedErrors;
+        }
+    }
+}

cb93d8cdc7834468b793e78c016a4895b1229c56
Jeff Kluge <jeffkl@microsoft.com>
2016-12-16 07:32:32 -0800

    Use System.Diagnostics.Process and System.Threading.Thread

--- src/MyTask/MyTask.cs
+++ src/MyTask/MyTask.cs
@@ -1,4 +1,7 @@
-﻿using Task = Microsoft.Build.Utilities.Task;
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Task = Microsoft.Build.Utilities.Task;
 
 namespace MyTasks
 {
@@ -9,6 +12,14 @@ namespace MyTasks
         public override bool Execute()
         {
             Log.LogMessage($"Hello from MyTask. Param1 = '{Param1}'");
+
+            TimeSpan sleepTime = TimeSpan.FromSeconds(3);
+
+            Log.LogMessage($"Sleeping for {sleepTime.TotalSeconds:N0} second(s)");
+            Thread.Sleep((int)sleepTime.TotalMilliseconds);
+            Log.LogMessage("Done");
+
+            Log.LogMessage($"There are {System.Diagnostics.Process.GetProcesses().Length} processes running");
 
             return !Log.HasLoggedErrors;
         }

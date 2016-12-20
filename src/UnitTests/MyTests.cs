cffe61a50ab81555024b292535432a8e3310aca0
Jeff Kluge <jeffkl@microsoft.com>
2016-12-20 13:27:42 -0800

    Add unit tests

---
+++ src/UnitTests/MyTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace UnitTests
+{
+    public class MyTests
+    {
+        [Fact]
+        public void CanRunUnitTests()
+        {
+            Assert.True(true);
+        }
+    }
+
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
